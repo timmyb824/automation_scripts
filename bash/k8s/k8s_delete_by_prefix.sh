beee54a9f33bd02ff5e05113d70827b6dad95c41
Tim Bryant <timothybryant3@gmail.com>
2024-01-28 10:28:55 -0500

    feat: add loki back remove promtail

--- bash/k8s/k8s_delete_by_prefix.sh
+++ bash/k8s/k8s_delete_by_prefix.sh
@@ -2,7 +2,7 @@
 
 # Set the name prefix of the pods you want to delete
 name_prefix="loki"
-namespace="monitoring"
+namespace="logging"
 
 # Use `kubectl` to get a list of pods with the given prefix and force delete them
 kubectl get pods --no-headers=true -n "$namespace" | awk '{if ($1 ~ /^'"$name_prefix"'/) print $1}' | xargs kubectl delete pod --force -n "$namespace"
